b4de3a6696ef838e557c9c3d544aac83072f741e
preston176 <prestonnyamweya@gmail.com>
2023-10-11 08:08:18 +0300

    nice nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import './Navbar.css'
+
+import React, { useEffect, useState } from 'react';
+import './Navbar.css';
 
 function Navbar() {
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolling(true);
+      } else {
+        setScrolling(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='navbar'>
+    <div className={`navbar ${scrolling ? 'scrolling' : ''}`}>
       <div className="navbar__left">
-        <a href="">Art<span>1906</span>_ <br />CREATIVE SHOPS</a>
+        <a href="">
+          Art<span>1906</span>_ <br />CREATIVE SHOPS
+        </a>
       </div>
       <div className="navbar__right">
         <ul>
           <li>Services</li>
           <li>Pricing</li>
-          <li>Resoures</li>
+          <li>Resources</li>
           <li>Contact</li>
           <li>Meet the Team</li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
